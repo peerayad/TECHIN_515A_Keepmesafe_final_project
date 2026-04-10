e1056aa8f691d437752f74b71d1cf8394fa66395
proudpeeraya <peerayad@uw.edu>
2026-04-09 23:26:18 -0700

    add test_camera.cpp fot testing camera

---
+++ KeepMeSafe_ML_Camera/test_camera.cpp
@@ -0,0 +1,80 @@
+//paste in main.cpp
+#include <Arduino.h>
+#include "esp_camera.h"
+
+#define PWDN_GPIO_NUM  -1
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  10
+#define SIOD_GPIO_NUM  40
+#define SIOC_GPIO_NUM  39
+#define Y9_GPIO_NUM    48
+#define Y8_GPIO_NUM    11
+#define Y7_GPIO_NUM    12
+#define Y6_GPIO_NUM    14
+#define Y5_GPIO_NUM    16
+#define Y4_GPIO_NUM    18
+#define Y3_GPIO_NUM    17
+#define Y2_GPIO_NUM    15
+#define VSYNC_GPIO_NUM 38
+#define HREF_GPIO_NUM  47
+#define PCLK_GPIO_NUM  13
+
+bool streaming = false;
+
+void setup() {
+  Serial.begin(921600);
+  delay(2000);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size   = FRAMESIZE_QVGA;  // 320x240
+  config.jpeg_quality = 12;
+  config.fb_count     = 2;
+
+  if (esp_camera_init(&config) != ESP_OK) {
+    Serial.println("ERR:CAM");
+    return;
+  }
+
+  Serial.println("READY");
+}
+
+void loop() {
+  if (Serial.available()) {
+    char cmd = Serial.read();
+    if (cmd == 'p') Serial.println("READY");
+    if (cmd == 's') streaming = true;
+    if (cmd == 'x') streaming = false;
+  }
+
+  if (streaming) {
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) return;
+
+    // ส่ง frame header: SIZE ตามด้วย bytes
+    uint32_t len = fb->len;
+    Serial.write((uint8_t*)&len, 4);   // 4 bytes = frame size
+    Serial.write(fb->buf, fb->len);    // JPEG bytes
+
+    esp_camera_fb_return(fb);
+  }
+}
